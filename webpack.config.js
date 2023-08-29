0a3478b445449e04ebf10840fb45dcac00bc8521
elibychkova <eli.bychkova@gmail.com>
2023-08-29 15:15:49 +0300

    added eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
